a587aed38e4c12e8987cbec087362770e2f767d0
Luan-art <luanlf2003@gmail.com>
2024-04-22 14:20:19 -0300

    Add project files.

---
+++ vogalConsoante/Program.cs
@@ -0,0 +1,31 @@
+﻿char[] palavra = new char[10];
+
+for (int i = 0; i < 10; i++)
+{
+    palavra[i] = char.Parse(Console.ReadLine());
+}
+
+Console.WriteLine();
+Console.WriteLine("Vogais");
+
+for (int i = 0; i < 10; i++)
+{
+
+    if (palavra[i] == 'a' || palavra[i] == 'e' || palavra[i] == 'i' || palavra[i] == 'o' || palavra[i] == 'u')
+    {
+        Console.WriteLine(palavra[i]);
+    }
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Consoantes");
+
+for (int i = 0; i < 10; i++)
+{
+
+    if (palavra[i] != 'a' && palavra[i] != 'e' && palavra[i] != 'i' && palavra[i] != 'o' && palavra[i] != 'u')
+    {
+        Console.WriteLine(palavra[i]);
+    }
+}
